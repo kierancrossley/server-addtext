d37d5156057aa8255566253cd884e73300da1541
Kieran Crossley <47427240+kierancrossley@users.noreply.github.com>
2024-10-22 03:42:03 +0100

    Add initial files

---
+++ sv_addtext/lua/autorun/sv_addtext.lua
@@ -0,0 +1,21 @@
+util.AddNetworkString("Kieran.AddText")
+
+Kieran = Kieran or {}
+
+function Kieran.AddText(ply, ...)
+    local args = {...}
+    net.Start("Kieran.AddText")
+    net.WriteUInt(#args, 4)
+    for k, v in pairs(args) do
+        if (k % 2) == 0 then
+            net.WriteString(v)
+        else
+            net.WriteColor(v)
+        end
+    end
+    if ply then
+        net.Send(ply)
+    else
+        net.Broadcast()
+    end
+end
